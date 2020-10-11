ae945256feef9de15a442b53855cf31506ee1e27
hyun <dhtmdgussksm@naver.com>
2020-10-11 18:34:34 +0900

    Update Auth.js
    delete not-activated auth way

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -9,10 +9,6 @@ const Auth =() => {
         let provider;
         if(name === "google"){
             provider = new firebaseInstance.auth.GoogleAuthProvider();
-        }else if(name ==="github"){
-            provider = new firebaseInstance.auth.GithubAuthProvider();
-        }else if(name ==="facebook"){
-            provider = new firebaseInstance.auth.FacebookAuthProvider();
         }
         await authService.signInWithPopup(provider);
     }
@@ -22,12 +18,6 @@ const Auth =() => {
         <div>
             <button onClick={onSocialClick} name="google">
                 Continue with Google
-            </button>
-            <button onClick={onSocialClick} name="github">
-                Continue with GitHub
-            </button>
-            <button onClick={onSocialClick} name="facebook">
-                Continue with FaceBook
             </button>
         </div>
     </div>
